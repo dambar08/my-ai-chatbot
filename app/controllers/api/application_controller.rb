71ba47e46f418b2a7af2fe5f451d37a89af6143e
dambar08 <notdambar@gmail.com>
2025-04-10 00:18:42 +0800

    refactor: separete api and not api responders

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,6 +1,17 @@
+require "api/application_responder"
 class Api::ApplicationController < ActionController::API
+  self.responder = Api::ApplicationResponder
+  include ActionController::MimeResponds
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   respond_to :json
 
-  include Authentication
+  # include ApiAuthentication
   include Pundit::Authorization
+
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    render nothing: true, status: 406 unless request.format == :json
+  end
 end
