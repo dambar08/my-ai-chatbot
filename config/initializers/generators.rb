1dbae104da91433794f68eba0f50762e1a0cbf9f
dambar08 <notdambar@gmail.com>
2025-04-09 23:07:11 +0800

    add: support for spec factory bot

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.generators do |g|
     g.orm :active_record
-    g.test_framework :rspec, fixture: false, views: true, fixture_replacement: :factory_bot
+    g.test_framework :rspec, fixture: false, views: true
+    g.fixture_replacement :factory_bot, dir: Rails.root.join("spec/factories")
   end
 end
